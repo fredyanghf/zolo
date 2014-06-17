18fdea5743198983801556f79ad8cc7ab23158de
Fred <Fred@Fred-PC>
2014-06-17 20:01:50 +0800

    product image

--- views/product/en/p1/intro.php
+++ views/product/en/p1/intro.php
@@ -1,6 +1,12 @@
-<div class="product_item_pic"><img id="pip" src="images/Power + Protection1 (Galaxy S5).jpg" /></div>
+<div class="product_item_pic"><img id="pip" src="images/power_s5_1.jpg" /></div>
 <div class="product_item_wrapper">
-    <div class="product_color small_color">  <span rel="images/Power + Protection1 (Galaxy S5).jpg" class="hover"><em></em><i>1</i></span> <span class="pc_black" rel="images/Power + Protection2 (Galaxy S5).jpg"><em></em><i>2</i></span> <span rel="images/Power + Protection3 (Galaxy S5).jpg"><em class="pc_blue"></em><i>3</i></span> <span rel="images/Power + Protection4 (Galaxy S5).jpg"><em class="pc_red"></em><i>4</i></span> </div>
+    <!--
+    <div class="product_color small_color">
+        <span rel="images/power_s5_1.jpg" class="hover"><em></em><i>1</i></span>
+        <span class="pc_black" rel="images/power_s5_2.jpg"><em></em><i>2</i></span>
+        <span rel="images/power_s5_3.jpg"><em class="pc_blue"></em><i>3</i></span>
+        <span rel="images/power_s5_4.jpg"><em class="pc_red"></em><i>4</i></span>
+    </div>-->
     <ul class="product_item_spec">
         <li>&bull; One charge to most smartphones.</li>
         <li>&bull; Magnetic cable clips work with most cords.</li>
@@ -8,7 +14,12 @@
         <li>&bull; Military grade protection case.</li>
         <li>&bull; Impact resistant screen protector.</li>
     </ul>
-    <div class="product_color big_color"> <span rel="images/Power + Protection1 (Galaxy S5).jpg" class="hover"><em></em><i>1</i></span> <span class="pc_black" rel="images/Power + Protection2 (Galaxy S5).jpg"><em></em><i>2</i></span> <span rel="images/Power + Protection3 (Galaxy S5).jpg"><em class="pc_blue"></em><i>3</i></span> <span rel="images/Power + Protection4 (Galaxy S5).jpg"><em class="pc_red"></em><i>4</i></span> </div>
+    <div class="product_color big_color">
+        <span rel="images/power_s5_1.jpg" class="hover"><em></em><i>1</i></span>
+        <span class="pc_black" rel="images/power_s5_2.jpg"><em></em><i>2</i></span>
+        <span rel="images/power_s5_3.jpg"><em class="pc_blue"></em><i>3</i></span>
+        <span rel="images/power_s5_4.jpg"><em class="pc_red"></em><i>4</i></span>
+    </div>
     <!--end.product_color-->
     <div class="product_price">$<strong>15</strong></div>
     <a class="product_buy" href="">CONTRIBUTE NOW</a> <span class="product_buy_info">Products not sold separately. </span>
